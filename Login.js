34fd59bdf6e7501af4dc9a18d159790f460fb1b9
NIKHIL GOSAIN <92264923+Nikhilgosain@users.noreply.github.com>
2023-11-30 19:19:56 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,17 @@
+function autoFill() {
+
+    var firstNameInput = document.getElementById("firstName");
+    var lastNameInput = document.getElementById("lastName");
+    var emailInput = document.getElementById("email");
+    var designationInput = document.getElementById("designation");
+    var addressInput = document.getElementById("Address");
+
+
+
+
+    firstNameInput.value = "Nikhil";
+    lastNameInput.value = "Gosain";
+    emailInput.value = "ngosainleo@gmail.com";
+    designationInput.value="Software-Engineer-Intern";
+    addressInput.value="Agra"
+}
